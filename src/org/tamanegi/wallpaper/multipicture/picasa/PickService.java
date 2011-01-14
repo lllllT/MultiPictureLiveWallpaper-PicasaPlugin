0697a9329e7cd8d39e4122bff5fed99685a118df
lllllT <lllllT@tamanegi.org>
2011-01-14 18:52:01 +0000

    up

--- src/org/tamanegi/wallpaper/multipicture/picasa/PickService.java
+++ src/org/tamanegi/wallpaper/multipicture/picasa/PickService.java
@@ -1,14 +1,30 @@
 package org.tamanegi.wallpaper.multipicture.picasa;
 
 import org.tamanegi.wallpaper.multipicture.plugin.LazyPickService;
+import org.tamanegi.wallpaper.multipicture.plugin.PictureContentInfo;
+import org.tamanegi.wallpaper.multipicture.plugin.ScreenInfo;
 
 public class PickService extends LazyPickService
 {
-
     @Override
     public LazyPicker onCreateLazyPicker()
     {
-        return null;
+        return new PicasaLazyPicker();
     }
 
+    private class PicasaLazyPicker extends LazyPicker
+    {
+        protected void onStart(String key, ScreenInfo hint)
+        {
+        }
+
+        protected void onStop()
+        {
+        }
+
+        public PictureContentInfo getNext()
+        {
+            return null;
+        }
+    }
 }
